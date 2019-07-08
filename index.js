7b6821b6c8369f082962d3a01dcfddbaf91c526a
Nathan Alex <nate745@gmail.com>
2019-07-08 01:25:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return string.toLowerCase() === "I can't hear you!"
+  if (string === string.toLowerCase()) {
+    console.log("I can't hear you!")
+  }
 }
